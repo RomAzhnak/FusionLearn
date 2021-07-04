fab4e40efa87bfe3e184566ca124b47866da3c05
RomAzhnak <ars-125@yandex.ru>
2021-07-04 12:01:06 +0300

    modified version

--- number_per_string.js
+++ number_per_string.js
@@ -1,27 +1,27 @@
 "use strict"
 let numberToWord = {
-   '-2': 'две',
-   '-1': 'одна',
+    '-2': 'две',
+    '-1': 'одна',
     '0': '',
-	'1': 'один',
-	'2': 'два',
-	'3': 'три',
-	'4': 'четыре',
-	'5': 'пять',
-	'6': 'шесть',
-	'7': 'семь',
-	'8': 'восемь',
-	'9': 'девять',
-	'10': 'десять',
-	'11': 'одиннадцать',
-	'12': 'двенадцать',
-	'13': 'тринадцать',
-	'14': 'четырнадцать' ,
-	'15': 'пятнадцать',
-	'16': 'шестнадцать',
-	'17': 'семнадцать',
-	'18': 'восемнадцать',
-	'19': 'девятнадцать',		
+    '1': 'один',
+    '2': 'два',
+    '3': 'три',
+    '4': 'четыре',
+    '5': 'пять',
+    '6': 'шесть',
+    '7': 'семь',
+    '8': 'восемь',
+    '9': 'девять',
+    '10': 'десять',
+    '11': 'одиннадцать',
+    '12': 'двенадцать',
+    '13': 'тринадцать',
+    '14': 'четырнадцать',
+    '15': 'пятнадцать',
+    '16': 'шестнадцать',
+    '17': 'семнадцать',
+    '18': 'восемнадцать',
+    '19': 'девятнадцать',
     '20': "двадцать",
     '30': "тридцать",
     '40': "сорок",
@@ -41,23 +41,23 @@ let numberToWord = {
     '900': "девятьсот"
 }
 
-function digitToString(digit, flag) {
+function digitToString(digit, thousands) {
     let out_string = [];
-    let hundreds = String(Math.floor(digit/100) * 100);
+    let hundreds = String(Math.floor(digit / 100) * 100);
     digit = digit % 100;
     out_string.push(String(numberToWord[hundreds]));
     if ((digit < 20) && (digit > 2)) {
         out_string.push(String(numberToWord[String(digit)]));
     } else {
-        out_string.push(String(numberToWord[String(Math.floor(digit/10)*10)]));
+        out_string.push(String(numberToWord[String(Math.floor(digit / 10) * 10)]));
         digit = digit % 10;
-        if ((digit < 3) && flag) {
+        if ((digit < 3) && thousands) {
             digit = -digit;
         }
         out_string.push(String(numberToWord[String(digit)]));
     }
-    if (flag) {
-        switch(digit) {
+    if (thousands) {
+        switch (digit) {
             case -1: out_string.push('тысяча');
                 break;
             case -2:
@@ -66,19 +66,26 @@ function digitToString(digit, flag) {
             case 4: out_string.push('тысячи');
                 break;
             default: out_string.push('тысяч');
-            
+
         }
     }
     return out_string;
 }
 
-let resultNumber = [];
-let number = 999999;    //ввод исходного числа
-let units = number % 1000;
-let thousands = Math.floor(number/1000);
-if (thousands) {
-    resultNumber = digitToString(thousands, true);
-} 
-resultNumber = [...resultNumber, ...digitToString(units, false)];
-resultNumber = resultNumber.join(' ').replace(/\s+/g,' ');
-console.log(resultNumber);
+function numberInWords(number) {
+    if (!number) { return 0 }
+    else {
+        let resultNumber = [];
+        let units = number % 1000;  // выделяем сотни,десятки,единицы
+        let thousands = Math.floor(number / 1000);  // выделяем тысячи
+        if (thousands) {
+            resultNumber = digitToString(thousands, true);
+        }
+        resultNumber = [...resultNumber, ...digitToString(units, false)];
+        resultNumber = resultNumber.join(' ').replace(/\s+/g, ' ');
+        return resultNumber;
+    }
+}
+
+// let number = 999999;    //ввод исходного числа
+console.log(numberInWords(555555));
